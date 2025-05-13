383f6af2c559adfd5756e28ebf7b2fbcb8148431
clement.horvilleur <clement.horvilleur@ecole-89.com>
2025-05-13 10:22:31 +0200

    commit

--- front/app/home.tsx
+++ front/app/home.tsx
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
 import NavBar from "../components/NavBar";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>Bienvenue sur la page Home !</Text>
       <NavBar />
     </View>
