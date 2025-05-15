0b0458ac0660a1e9a1987af41a553bc425ab0033
clement.horvilleur <clement.horvilleur@ecole-89.com>
2025-05-15 10:33:29 +0200

    commit

--- front/App.tsx
+++ front/App.tsx
@@ -5,6 +5,7 @@ import Home from "./app/home";
 import Produit from "./app/produit";
 import Panier from "./app/panier";
 import Profil from "./app/profil";
+import Quiz from "./app/quiz";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +20,7 @@ export default function App() {
         <Stack.Screen name="Produit" component={Produit} />
         <Stack.Screen name="Panier" component={Panier} />
         <Stack.Screen name="Profil" component={Profil} />
+        <Stack.Screen name="Quiz" component={Quiz} />
       </Stack.Navigator>
     </NavigationContainer>
   );
